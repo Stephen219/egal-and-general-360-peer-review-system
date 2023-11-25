89935199cbea035ac85a91c336f65a0a5373283d
Wojciech Lewczuk <LewczukW@cardiff.ac.uk>
2023-11-25 17:24:57 +0000

    added foundation code for role based access

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
 delete from users;
 insert into users (id, username, password) values (1,'user3', 'password');
 insert into users (id, username, password) values (2,'user4', 'password');
+
+insert into roles (role_id, name)
+values (1, 'ADMIN');
+insert into roles (role_id, name)
+values (2, 'USER');
+
+insert into users_roles (username, role_id)
+values ('admin', 1);
+insert into users_roles (username, role_id)
+values ('admin', 2);
+insert into users_roles (username, role_id)
+values ('user', 2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,11 +10,34 @@ Create TABLE IF NOT EXISTS form_requests (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (Id)
 );
+
 drop table if exists users;
 create table if not exists users (
     id int not null auto_increment primary key,
     username varchar(255) not null,
     password varchar(255) not null
 )engine=InnoDB;
+
+drop table if exists roles;
+CREATE TABLE if not exists roles
+(
+    role_id int(11)     NOT NULL AUTO_INCREMENT primary key,
+    name    varchar(45) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE if not exists users_roles
+(
+    id       bigint auto_increment primary key,
+    username varchar(50) NOT NULL,
+    role_id  int(11)     NOT NULL
+) engine = InnoDB;
+
+CREATE VIEW if not exists user_authorities as
+select u.username as username, CONCAT("ROLE_", r.name) as authority
+from users u
+         inner join users_roles ur on u.username = ur.username
+         inner join roles r on ur.role_id = r.role_id;
+
+
 
 
