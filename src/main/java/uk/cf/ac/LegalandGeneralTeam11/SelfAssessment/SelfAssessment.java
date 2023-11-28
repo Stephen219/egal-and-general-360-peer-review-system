3fe641f1386f4e071a125de01a1fb296babd8056
c21014741 <harrisont7@cardiff.ac.uk>
2023-11-28 14:38:08 +0000

    fixed branch, saves to class

---
+++ src/main/java/uk/cf/ac/LegalandGeneralTeam11/SelfAssessment/SelfAssessment.java
@@ -0,0 +1,37 @@
+package uk.cf.ac.LegalandGeneralTeam11.SelfAssessment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SelfAssessment {
+    private Long id;
+    private String oneOne;
+    private String oneTwo;
+    private String oneThree;
+    private String oneFour;
+    private String twoOne;
+    private String twoTwo;
+    private String twoThree;
+    private String twoFour;
+    private String threeOne;
+    private String threeTwo;
+    private String threeThree;
+    private String threeFour;
+    private String fourOne;
+    private String fourTwo;
+    private String fourThree;
+    private String fourFour;
+    private String fiveOne;
+    private String fiveTwo;
+    private String fiveThree;
+    private String fiveFour;
+    private String fiveFive;
+    private String sixOne;
+    private String sixTwo;
+    private String sixThree;
+    private String sixFour;
+    private String superpower;
+    private String impact;
+}
