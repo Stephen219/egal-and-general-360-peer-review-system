b05f0d0c13113e251235cf5c7c2dfac9fc2c6788
Wojciech Lewczuk <LewcukW@cardiff.ac.uk>
2023-12-07 17:12:34 +0000

    bug fix on displaying json results

--- src/main/java/uk/cf/ac/LegalandGeneralTeam11/Graphs/GraphController.java
+++ src/main/java/uk/cf/ac/LegalandGeneralTeam11/Graphs/GraphController.java
@@ -1,6 +1,8 @@
 package uk.cf.ac.LegalandGeneralTeam11.Graphs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,13 +21,18 @@ public class GraphController{
     @Autowired
     FormService formservice;
 
-    @GetMapping("/account/{id}/graphs")
-    public List<Map<String, Object>> getCategoryAverages(@PathVariable String id) {
-        Form form = formservice.getFormById(id);
-        return graphservice.getCategoryAverages(id);
+
+    @GetMapping("/account/averages/{id}")
+    public ResponseEntity<List<Map<String, Object>>> getCategoryAverages(@PathVariable String id) {
+        //returns a list of category averages
+        List<Map<String, Object>> categoryAverages =graphservice.getCategoryAverages(id);
+
+        if (categoryAverages.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(categoryAverages, HttpStatus.OK);
+        }
     }
-
-
 //        ModelAndView modelAndView = new ModelAndView("account/graphs", "graphs", graphservice.getCategoryAverages(formid));
 //        modelAndView.addObject("form", form);
 //        return modelAndView;
