762c448dfd71b71bf7efcc49cf76ca289402c9d2
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2023-12-12 11:13:57 +0000

    tables

--- src/main/java/uk/cf/ac/LegalandGeneralTeam11/answers/AnswerRepo.java
+++ src/main/java/uk/cf/ac/LegalandGeneralTeam11/answers/AnswerRepo.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -80,6 +81,159 @@ public class AnswerRepo {
     }
 
 
+    /**
+     * this is the method that gets the answers for a question for a form for a user and returns them in a map
+     * @param responder
+     * @param formId
+     * @return
+     */
+
+
+
+
+
+
+    public Map<String, String> getFormAnswersForaUser(String responder, String formId) {
+        String sql = "SELECT q.question, " +
+                "CASE " +
+                "    WHEN a.answer = 1 THEN 'Never' " +
+                "    WHEN a.answer = 2 THEN 'Rarely' " +
+                "    WHEN a.answer = 3 THEN 'Sometimes' " +
+                "    WHEN a.answer = 4 THEN 'Often' " +
+                "    WHEN a.answer = 5 THEN 'Always' " +
+                "    ELSE a.answer " +
+                "END AS descriptive_answer " +
+                "FROM questions q " +
+                "JOIN answers a ON q.Id = a.question_id " +
+                "WHERE a.responder = ? AND a.form_id = ?";
+
+        try {
+            Map<String, String> result = new HashMap<>();
+
+            jdbcTemplate.query(sql, (rs) -> {
+                String question = rs.getString("question");
+                String descriptiveAnswer = rs.getString("descriptive_answer");
+                result.put(question, descriptiveAnswer);
+            }, responder, formId);
+
+            return result;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+    }
+
+
+
+
+
+    public Map<String, List<String>> getQuestionAnswersUser(String responder, String formId) {
+        String sql = "SELECT " +
+                "CASE " +
+                "    WHEN q.category = 'textarea' THEN 'open ended' " +
+                "    ELSE q.category " +
+                "END AS category, " +
+                "q.question, " +
+                "CASE " +
+                "    WHEN a.answer = 1 THEN 'Never' " +
+                "    WHEN a.answer = 2 THEN 'Rarely' " +
+                "    WHEN a.answer = 3 THEN 'Sometimes' " +
+                "    WHEN a.answer = 4 THEN 'Often' " +
+                "    WHEN a.answer = 5 THEN 'Always' " +
+                "    ELSE a.answer " +
+                "END AS descriptive_answer " +
+                "FROM questions q " +
+                "JOIN answers a ON q.Id = a.question_id " +
+                "WHERE a.responder = ? AND a.form_id = ? " +
+                "ORDER BY q.category, q.question";
+
+        try {
+            List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql, responder, formId);
+
+            // Grouping by category
+            Map<String, List<String>> resultMap = new HashMap<>();
+            String currentCategory = null;
+            List<String> currentList = null;
+
+            for (Map<String, Object> row : rows) {
+                String category = (String) row.get("category");
+                String descriptiveAnswer = (String) row.get("descriptive_answer");
+
+                if (!category.equals(currentCategory)) {
+                    // New category encountered
+                    if (currentList != null) {
+                        resultMap.put(currentCategory, currentList);
+                    }
+
+                    currentCategory = category;
+                    currentList = new ArrayList<>();
+                }
+
+                currentList.add(descriptiveAnswer);
+            }
+
+            // Adding the last category
+            if (currentList != null) {
+                resultMap.put(currentCategory, currentList);
+            }
+
+            return resultMap;
+        } catch (Exception e) {
+            // Handle any errors here
+            e.printStackTrace();
+            return new HashMap<>(); // Return an empty map or handle the error accordingly
+        }
+    }
+//public Map<String, List<String>> getQuestionAnswersUser(String responder, String formId) {
+//    String sql = "SELECT " +
+//            "  CASE " +
+//            "    WHEN q.category = 'textarea' THEN 'open ended ' " +
+//            "    ELSE q.category " +
+//            "  END AS category, " +
+//            "  q.question, " +
+//            "  CASE " +
+//            "    WHEN a.answer = 1 THEN 'Never' " +
+//            "    WHEN a.answer = 2 THEN 'Rarely' " +
+//            "    WHEN a.answer = 3 THEN 'Sometimes' " +
+//            "    WHEN a.answer = 4 THEN 'Often' " +
+//            "    WHEN a.answer = 5 THEN 'Always' " +
+//            "    ELSE NULL " +
+//            "  END AS descriptive_answer " +
+//            "FROM " +
+//            "  questions q " +
+//            "JOIN " +
+//            "  answers a ON q.Id = a.question_id " +
+//            "WHERE " +
+//            "  a.responder = ? AND a.form_id = ? " +
+//            "GROUP BY q.category, q.question";
+//
+//    try {
+//        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql, responder, formId);
+//
+//        Map<String, List<String>> result = new HashMap<>();
+//
+//        for (Map<String, Object> row : rows) {
+//            String category = (String) row.get("category");
+//            String question = (String) row.get("question");
+//            String descriptiveAnswer = (String) row.get("descriptive_answer");
+//
+//            // Check if the category is already in the result map
+//            if (!result.containsKey(category)) {
+//                result.put(category, new ArrayList<>());
+//            }
+//
+//            // Add the descriptive answer to the list under the category
+//            result.get(category).add(question + ": " + descriptiveAnswer);
+//        }
+//
+//        return result;
+//    } catch (Exception e) {
+//        // Handle any errors here
+//        e.printStackTrace();
+//        return new HashMap<>(); // Return an empty map or handle the error accordingly
+//    }
+//}
 
 
 
--- src/main/java/uk/cf/ac/LegalandGeneralTeam11/answers/AnswerServiceInter.java
+++ src/main/java/uk/cf/ac/LegalandGeneralTeam11/answers/AnswerServiceInter.java
@@ -3,8 +3,11 @@ package uk.cf.ac.LegalandGeneralTeam11.answers;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 public interface AnswerServiceInter {
     @Autowired
     public void processAndSaveAnswers(List<Answer> answerList);
+    public Map<String, String> getFormAnswersForaUser(String responder, String formId);
+    public Map<String, List<String>> getQuestionAnswersUser(String responder, String formId);
 }
