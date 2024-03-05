43cce4108f5b6a2b98bddeaa14f617f7f16a1662
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2024-03-05 18:51:57 +0000

    modifications for jenkins

--- src/test/java/uk/cf/ac/LegalandGeneralTeam11/FormRequestandFormTests/FormrequestTestProcess.java
+++ src/test/java/uk/cf/ac/LegalandGeneralTeam11/FormRequestandFormTests/FormrequestTestProcess.java
@@ -5,6 +5,7 @@ package uk.cf.ac.LegalandGeneralTeam11.FormRequestandFormTests;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,12 +24,16 @@ import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 
 public class FormrequestTestProcess {
 
     private WebDriver driver;
+
+
 
     /**
      * Sets up the ChromeDriver and navigates to the login page.
@@ -42,21 +47,40 @@ public class FormrequestTestProcess {
         options.addArguments("--ignore-certificate-errors");
         // make the browser headless to avoid opening a browser window during the test run on the CI server (Jenkins) and the local machine
         options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
+
         driver = new ChromeDriver(options);
     }
     /**
      * Tests that a user can login and access the dashboard.
      */
 
+    // getting the url port and the prot of the server
+
+
+
+    public String buildUrl(HttpServletRequest request) {
+        String protocol = request.getScheme();
+        String webAddress = request.getServerName();
+        int port = request.getServerPort();
+        return protocol + "://" + webAddress + ":" + port;
+    }
+
+
+
+
+
     // TODO//: while worKing in this TEST i was in a  branch that is behind  the user wer default user and password wer default password
     // later we change it to Henry and password user
 
     @Test
     @WithMockUser(username = "test@gmail.com", password = "user", roles = "USER")
-    public void testLoginAndAccessDashboard() {
+    public void testLoginAndAccessDashboard(
+
+    ) {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        String url = buildUrl(request);
         String pageSource = driver.getPageSource();
-        driver.get("https://localhost:8443/login");
+        driver.get(url + "/login");
         WebElement usernameInput = driver.findElement(By.name("username"));
         WebElement passwordInput = driver.findElement(By.name("password"));
         usernameInput.sendKeys("test@gmail.com");
@@ -65,7 +89,8 @@ public class FormrequestTestProcess {
         loginForm.submit();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.urlContains("/account"));
-        assertEquals("https://localhost:8443/account", driver.getCurrentUrl()); // check that the user is redirected to the dashboard
+        assertEquals(url + "/account", driver.getCurrentUrl()); // check that the user is redirected to the dashboard
+                // check that the user is redirected to the dashboard
         WebElement toggleButton = driver.findElement(By.xpath("//button[contains(text(), 'Request Form')]")); //requests a form
         toggleButton.click();
         WebDriverWait popupWait = new WebDriverWait(driver, Duration.ofSeconds(15));
@@ -76,21 +101,25 @@ public class FormrequestTestProcess {
         //WebElement flashMessage = flashWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='flashMessage' and contains(text(), 'your form request has been submited!')]")));
         WebElement flashMessage = flashWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='flashMessage']")));
         assertEquals("your form request has been submited! Cancel", flashMessage.getText());
-        assertEquals("https://localhost:8443/account", driver.getCurrentUrl());
+        assertEquals(url+ "/account", driver.getCurrentUrl());
         assertTrue(driver.getPageSource().contains(" <div id=\"chartContainer\" class=\"bg-white p-4 rounded-md shadow flex mb-4\">"));
 
         assertTrue(driver.getPageSource().contains("<a href=\"/get_reviewers/form1\">"));
-        driver.get("https://localhost:8443/get_reviewers/form1");
+        driver.get(url + "/get_reviewers/form1");
         assertTrue(driver.getPageSource().contains("<form id=\"form\" class=\"w-full max-w-sm bg-white flex flex-col py-5 px-8 rounded-lg shadow-lg m-6 \" action=\"/submit_reviewers/form1\" method=\"post\">"));
         // the above checks whether the submit reviewers is rendered fully
-        driver.get("https://localhost:8443/review/form1");
-        driver.get("https://localhost:8443/logout");
+        driver.get(url + "/review/form1");
+        driver.get(url + "/logout");
     }
     @Test
     @WithMockUser(username = "admin@gmail.com", password = "admin", roles = "ADMIN")
-    public void testLoginAndAccessDashboardForAdmin() {
+    public void testLoginAndAccessDashboardForAdmin(
+
+    ) {
+        HttpServletRequest request = mock(HttpServletRequest.class);
         String pageSource = driver.getPageSource();
-        driver.get("https://localhost:8443/login");
+        String url = buildUrl(request);
+        driver.get(url + "/login");
         WebElement usernameInput = driver.findElement(By.name("username"));
         WebElement passwordInput = driver.findElement(By.name("password"));
         usernameInput.sendKeys("admin@gmail.com");
@@ -99,23 +128,19 @@ public class FormrequestTestProcess {
         loginForm.submit();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.urlContains("/admin"));
-        assertEquals("https://localhost:8443/admin", driver.getCurrentUrl()); // check that the user is redirected to the dashboard
+        assertEquals(url+ "/admin", driver.getCurrentUrl()); // check that the user is redirected to the dashboard
         //System.out.println(driver.getPageSource());
         assertTrue(driver.getPageSource().contains("<a class=\"inline-flex items-center w-full text-sm font-semibold text-white transition-colors duration-150 cursor-pointer hover:text-green-500\" href=\"admin/all_forms\">"));
         assertTrue(driver.getPageSource().contains("<a class=\"inline-flex items-center w-full text-sm font-semibold text-white transition-colors duration-150 cursor-pointer hover:text-green-500\" href=\"/admin/questions\">"));
-        driver.get("https://localhost:8443/admin/all_forms");
+        driver.get(url + "/all_forms");
         assertTrue(driver.getPageSource().contains("<h1 class=\"text-2xl font-bold text-center md:text-center\">All forms</h1>"));
-        assertEquals("https://localhost:8443/admin/all_forms", driver.getCurrentUrl());
-        driver.get("https://localhost:8443/admin/questions");
-        assertEquals("https://localhost:8443/admin/questions", driver.getCurrentUrl());
-        driver.get("https://localhost:8443/domain");
-        assertEquals("https://localhost:8443/domain", driver.getCurrentUrl());
-        driver.get("https://localhost:8443/logout");
-        driver.get("https://localhost:8443/review/form1");
-        System.out.println(driver.getPageSource());
-
-
-
+        assertEquals(url + "/admin/all_forms", driver.getCurrentUrl());
+        driver.get(url + "/admin/questions");
+        assertEquals(url + "/admin/questions", driver.getCurrentUrl());
+        driver.get(url + "/domain");
+        assertEquals(url + "/domain", driver.getCurrentUrl());
+        driver.get( url +"/logout");
+        driver.get(url + "/review/form1");
 
 
     }
